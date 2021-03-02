2e6158930f3806ac63c4c82219b83a963963c17d
Jordan Del Colle <jadelcolle@gmail.com>
2021-03-01 18:43:49 -0700

    responsive menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// js code to trigger the button goes here
+// Step 1: query(search) the document and select the element the user will click on to make the nav show and hide
+const button = document.querySelector('.hamburger-btn');
+
+// Step 2: place a click event on the button
+button.addEventListener('click', () =>{
+    // when the button is clicked, grab the nav element and add the special class
+    document.querySelector('nav').classList.toggle('show-nav');
+});
+
+// note: when placing a class name inside the classList MSInputMethodContext, do not include the . It will already expect a class name.
+
